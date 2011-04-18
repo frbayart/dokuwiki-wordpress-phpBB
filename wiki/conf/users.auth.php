6fb1464d7268e6e625ad35fce144d287eeefedd1
Francois BAYART <francois.bayart@f5w.org>
2011-04-19 00:40:52 +0200

    initial releas

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# user:MD5password:Real Name:email:groups,comma,seperated
+
+
+frbayart:afaecf6aa3270207531354afa3b272e3:François BAYART:francois.bayart@f5w.org:admin,user
